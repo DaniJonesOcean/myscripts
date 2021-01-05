6dc50bddbcb578f25f834bd6286bd4052ee9ef7e
Dan Jones <dcjones.work@gmail.com>
2021-01-05 14:33:34 +0000

    Added positional argument

--- ecse_ecco_check.sh
+++ ecse_ecco_check.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+#
+# -- This command accepts a filename as the first positional argument
+# --- Otherwise it will try to use STDOUT.0000 as the filename
+#
+
 ###########################################
 # Script to check ECCOv4-r4 runs for eCSE #
 ###########################################
@@ -10,8 +15,17 @@ echo " "
 echo "Checking ECCOv4-r4 run (eCSE configuration)"
 echo " "
 
+# if first argument empty, use default filename
+if test -z "$1" 
+then
+    echo "Using default filename STDOUT.0000"
+    filename=STDOUT.0000
+else
+    filename=$1
+fi
+
 # check the last line of standard output
-lastline=$(tail -1 STDOUT.0000)
+lastline=$(tail -1 $filename)
 lastline_ok='PROGRAM MAIN: Execution ended Normally'
 if [[ "$lastline" == "$lastline_ok" ]]; then
     echo "MITgcm (ECCOv4-r4) execution ended normally"
@@ -22,51 +36,51 @@ echo " "
 echo "-----"
 
 # check cost function
-echo "-----"
-echo "Total cost function value is given by fc "
-head -1 costfunction0129
-echo "-----"
+#echo "-----"
+#echo "Total cost function value is given by fc "
+#head -1 costfunction0129
+#echo "-----"
 
 # min temperature values
 echo "-----"
 echo "Global minimum potential temperature (deg C)" 
-grep _theta_min STDOUT.0000 | cut -c 58-80
+grep _theta_min $filename | cut -c 58-80
 echo "-----" 
 
 # mean temperature values
 echo "-----"
 echo "Global mean potential temperature (deg C)" 
-grep dynstat_theta_mean STDOUT.0000 | cut -c 58-80
+grep dynstat_theta_mean $filename | cut -c 58-80
 echo "-----" 
 
 # max temperature values
 echo "-----"
 echo "Global maximum potential temperature (deg C)" 
-grep _theta_max STDOUT.0000 | cut -c 58-80
+grep _theta_max $filename | cut -c 58-80
 echo "-----" 
 
 # min salinity values
 echo "-----"
 echo "Global minimum salinity (psu)" 
-grep _salt_min STDOUT.0000 | cut -c 58-80
+grep _salt_min $filename | cut -c 58-80
 echo "-----" 
 
 # mean salinity values
 echo "-----"
 echo "Global mean salinity (psu)" 
-grep dynstat_salt_mean STDOUT.0000 | cut -c 58-80
+grep dynstat_salt_mean $filename | cut -c 58-80
 echo "-----" 
 
 # max salinity values
 echo "-----"
 echo "Global maximum salinity (psu)" 
-grep _salt_max STDOUT.0000 | cut -c 58-80
+grep _salt_max $filename | cut -c 58-80
 echo "-----" 
 
 # global mean sea ice area
 echo "-----"
 echo "Global mean sea ice area (m^2/m^2)" 
-grep seaice_area_mean STDOUT.0000 | cut -c 57-90
+grep seaice_area_mean $filename | cut -c 57-90
 echo "-----" 
 
 # profiling
@@ -74,30 +88,30 @@ echo " "
 echo " -------------- PROFILING ----------------- "
 echo " "
 
-grep -A 3 "Seconds in section" STDOUT.0000 | cut -c 23-90
+grep -A 3 "Seconds in section" $filename | cut -c 23-90
 
 echo " "
 echo " "
 
-echo " "
-echo " -------------- PROFILING (ADJOINT) --------------- "
-echo " "
+#echo " "
+#echo " -------------- PROFILING (ADJOINT) --------------- "
+#echo " "
 
-echo "-----"
-echo "Mean sensitivity to temperature"
-grep ad_dynstat_adtheta_mean STDOUT.0000 | cut -c 57-90
-echo "-----"
+#echo "-----"
+#echo "Mean sensitivity to temperature"
+#grep ad_dynstat_adtheta_mean $filename | cut -c 57-90
+#echo "-----"
 
-echo "-----"
-echo "Mean sensitivity to salinity"
-grep ad_dynstat_adsalt_mean STDOUT.0000 | cut -c 57-90
-echo "-----"
+#echo "-----"
+#echo "Mean sensitivity to salinity"
+#grep ad_dynstat_adsalt_mean $filename | cut -c 57-90
+#echo "-----"
 
-echo " -------------- ******************* --------------- "
+#echo " -------------- ******************* --------------- "
 
 echo "-----" 
 echo "Total wall-clock time (seconds)"
-grep -m 1 -h -A 3 "Seconds in section" STDOUT.0000 | cut -c 23-90| tail -n 1 | cut -c 18-80
+grep -m 1 -h -A 3 "Seconds in section" $filename | cut -c 23-90| tail -n 1 | cut -c 18-80
 echo "-----" 
 
 # end formatting
